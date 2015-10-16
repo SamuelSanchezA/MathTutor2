dc7c2587cca3b5e01c1fdd81e39ed50c23e6c9fe
ChanelAquino <chanelaquino@student.hartnell.edu>
2015-10-16 10:16:53 -0700

    Math Tutor 2: Initialized function prototypes

---
+++ mathTutor2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+
+char menu();            // Sam
+/*  Summary: 
+    Precondition:
+    Postcondition:  */
+
+int addition();         // Sam
+/*  Summary:
+    Precondition:
+    Postcondition:  */
+    
+int multiplication();   // Sam
+/*  Summary:
+    Precondition:
+    Postcondition:  */
+    
+int subtraction();      // Giselle
+/*  Summary:
+    Precondition:
+    Postcondition:  */
+    
+int division();         // Chanel
+/*  Summary:
+    Precondition:
+    Postcondition:  */
+    
+string compliment();    // Giselle
+/*  Summary:
+    Precondition:
+    Postcondition:  */
+    
+string encourage();     // Giselle
+/*  Summary:
+    Precondition:
+    Postcondition:  */
+
+
+int main()
+{
+
+}
